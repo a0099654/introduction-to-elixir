d01f3b51f5e9c1f6e7b963f4eb16c8db2936ffea
Stella Widyasari <stella.widyasari@gmail.com>
2017-12-22 16:34:33 +0800

    [Stella]Adding functions on expenses to use HTTPPoison and Poison

--- first_elixir_project/lib/expenses.ex
+++ first_elixir_project/lib/expenses.ex
@@ -2,6 +2,36 @@ defmodule Expenses do
   def current_balance(initial, spending) do
     initial - spending
   end
+
+  def total_spendings_FL(amount) do
+    url = "localhost:3000/taxes"
+    case HTTPoison.get(url) do
+      {:ok, response} -> parse(response) |> calculate(amount)
+      {:error, _} -> "Error fetching tax rates"
+    end
+  end
+
+  defp parse(%{status_code: 200, body:   }) do
+    
+  end
+
+  defp calculate({:ok, rates}, amount) do
+    tax_rate = find_tax(rates)
+    amount + (amount * tax_rate)
+  end
+
+  defp find_tax([%{  } | _ ]) do
+    rate
+  end
+
+  defp find_tax([_ | tail]) do
+    
+  end
+
+  defp find_tax([]) do
+    raise "FL rate not found"
+  end
+
 end
 
 #mix run -e "Expenses.current_balance(100,20)|>IO.puts"
