43df70c91cea972cb9a8b3b7a536219957aad051
Stella Widyasari <stella.widyasari@gmail.com>
2017-12-22 16:03:14 +0800

    [Stella]Initialise a generated elixir projec with mix build tools

---
+++ first_elixir_project/lib/first_elixir_project.ex
@@ -0,0 +1,18 @@
+defmodule FirstElixirProject do
+  @moduledoc """
+  Documentation for FirstElixirProject.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FirstElixirProject.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
